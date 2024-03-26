9e9151ea6e7800f26d12a92bc01c57d275338963
auseckas <andrew@threatx.io>
2024-03-25 21:34:18 -0600

    Dockerfile, bugfixes

--- run.sh
+++ run.sh
@@ -2,12 +2,12 @@
 log_level=$1
 sensor_name=$2
 
-# mount -t tracefs tracefs /sys/kernel/tracing/
+mount -t tracefs tracefs /sys/kernel/tracing/
 if ! grep -qs 'tracefs' /proc/mounts; then
     echo 'Could not mount in tracefs. This could impact functionality. Please make sure the container was started with --privileged flag.'
 fi
 
-# mount -t securityfs securityfs /sys/kernel/security
+mount -t securityfs securityfs /sys/kernel/security
 if ! grep -qs 'securityfs' /proc/mounts; then
     echo "Could not mount in securityfs. This could will impact LSM functionality. Please make sure the container was started with --privileged flag."
 fi
